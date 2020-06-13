9376266cceef92063224eb90ab978a8c235b6d16
Joshua Moore <morejoshua99@hotmail.com>
2020-06-13 21:49:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
-
-
+#use pry to view values of variables and where you are placing the values
+require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key , value),
